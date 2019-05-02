620cf9f0cdc56bf3427764068e272be8655a21dd
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-05-01 23:49:39 -0400

    Update BuggyClassTestBC2.java

--- Assignment7 (White Box Testing)/Test/edu/qc/seclass/BuggyClassTestBC2.java
+++ Assignment7 (White Box Testing)/Test/edu/qc/seclass/BuggyClassTestBC2.java
@@ -9,6 +9,6 @@ class BuggyClassTestBC2 {
     //This does greater than 50% branch coverage and only shows the fault
     @Test
     public void buggyMethod2() {
-        assertEquals(5, BuggyClass.buggyMethod2(2, 6));
+        assertEquals(5, BuggyClass.buggyMethod2(6, 2));
     }
 }
