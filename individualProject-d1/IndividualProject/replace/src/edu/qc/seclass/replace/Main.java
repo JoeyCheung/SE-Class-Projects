7f0361cda74ccd9634b9deb2837955aa0cb35266
Joey Cheung <joeycheung0@gmail.com>
2019-05-13 22:37:17 -0400

    Added deliverable 1

---
+++ individualProject-d1/IndividualProject/replace/src/edu/qc/seclass/replace/Main.java
@@ -0,0 +1,13 @@
+package edu.qc.seclass.replace;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO: Empty skeleton method
+    }
+
+    private static void usage() {
+        System.err.println("Usage: Replace [-b] [-f] [-l] [-i] <from> <to> -- " + "<filename> [<filename>]*" );
+    }
+
+}
